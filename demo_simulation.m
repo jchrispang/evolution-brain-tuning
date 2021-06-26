67760a453a5c6a70b04386c26cfd7f44dd7e7f28
JamesPang <james.pang1@monash.edu>
2021-06-27 09:20:24 +1000

    update demo_simulation

--- demo_simulation.m
+++ demo_simulation.m
@@ -1,14 +1,19 @@
 % demo_simulation.m
+% 
+% Matlab code to demonstrate simulation of the models
 
-%% load connectome data
+%% LOAD SOME CONNECTOME MATRICES
 
 load('data/connectome_human.mat')
 load('data/connectome_chimp.mat')
 load('data/connectome_macaque.mat')
 load('data/connectome_marmoset.mat')
 
+%% DEMO FOR REDUCED WONG-WANG MODEL
 
-%% reduced Wong-Wang model: generating and analyzing time series
+% =========================================================================
+%                  generating and analyzing time series
+% =========================================================================
 
 % load predefined reduced Wong-Wang model parameters
 param = utils.loadParameters_reducedWongWang_func;
@@ -69,30 +74,12 @@ plot(1:param.N, tau, '.-')
 xlabel('region')
 ylabel('timescale, \tau')
 
-
-%% reduced Wong-Wang model: calculating and analyzing synaptic gating tuning curve  
-   
-% load predefined reduced Wong-Wang model parameters
-param = utils.loadParameters_reducedWongWang_func;
-
-% define connectome matrix A
-type = 'human';
-param.A = eval(sprintf('connectome_%s', type));  % replace with your own connectivity matrix
-param.N = size(param.A, 2);
-
-% normalize connectivity matrix with respect to maximum weight
-normalization = 'maximum';
-param.A = utils.norm_matrix(param.A, normalization);  
-
-% define simulation time 
-tpre =  100;                                     % burn time to remove transient
-tpost = 50;                                      % steady-state time (it is advisable to increase this to reach ergodicity)
-param.tmax = tpre + param.tstep + tpost;         
-param.tspan = [0, param.tmax];
-param.T = 0:param.tstep:param.tmax;
+% =========================================================================
+%          calculating and analyzing synaptic gating tuning curve
+% =========================================================================
 
 % define vector of recurrent connection strengths and number of trials
-w_vec = 0.05:0.05:1;                             % make sure to increase resolution for a proper analysis
+w_vec = 0.05:0.05:1;  % make sure to increase resolution for a proper analysis
 num_trials = 1;
 
 % calculate tuning curve per region
@@ -115,7 +102,11 @@ xlabel('region')
 ylabel('dynamic range')
 
 
-%% reduced Wilson-Cowan model: generating time series
+%% DEMO FOR WILSON-COWAN MODEL
+
+% =========================================================================
+%                          generating time series
+% =========================================================================
 
 % load predefined Wilson-Cowan model parameters
 param = utils.loadParameters_WilsonCowan_func;
@@ -153,37 +144,19 @@ plot(T_steady, S_E_steady)
 xlabel('time')
 ylabel('excitatory firing rate, S_E')
 
-
-%% Wilson-Cowan model: calculating and analyzing synaptic gating tuning curve  
-   
-% load predefined Wilson-Cowan model parameters
-param = utils.loadParameters_WilsonCowan_func;
-
-% define connectome matrix A
-type = 'human';
-param.A = eval(sprintf('connectome_%s', type));  % replace with your own connectivity matrix
-param.N = size(param.A, 2);
-
-% normalize connectivity matrix with respect to maximum weight
-normalization = 'maximum';
-param.A = utils.norm_matrix(param.A, normalization);  
-
-% define simulation time 
-tpre =  5;                                     % burn time to remove transient
-tpost = 0.2;                                   % steady-state time (it is advisable to increase this to reach ergodicity)
-param.tmax = tpre + param.tstep + tpost;         
-param.tspan = [0, param.tmax];
-param.T = 0:param.tstep:param.tmax;
+% =========================================================================
+%       calculating and analyzing excitatory firing rate tuning curve
+% =========================================================================
 
 % define vector of excitatory to excitatory connection strengths and number of trials
-wEE_vec = 1:1:30;                              % make sure to increase resolution for a proper analysis
+wEE_vec = 1:1:30;  % make sure to increase resolution for a proper analysis
 num_trials = 1;
 
 % calculate tuning curve per region
 [SEmean, SImean] = utils.calc_tuning_WilsonCowan(param, wEE_vec, tpre, num_trials);
 
 % calculate some stats of SEmean
-S_transition = 0.15;                           % transition value of Smean
+S_transition = 0.15;                           % transition value of SEmean
 stats = utils.calc_response_stats(wEE_vec, SEmean, S_transition);
 
 % plot synaptic gating tuning curve and dynamic range per region
@@ -199,7 +172,11 @@ xlabel('region')
 ylabel('dynamic range')
 
 
-%% drift diffusion model: generating time series
+%% DEMO FOR DRIFT DIFFUSION MODEL
+
+% =========================================================================
+%                          generating time series
+% =========================================================================
 
 % load predefined drift diffusion model parameters
 param = utils.loadParameters_driftDiffusion_func;
